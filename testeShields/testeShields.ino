4e0e05cee4d667e1a2df308058cefe90c4b84f92
lara2058 <lara2058@hotmail.com>
2019-10-05 17:22:48 -0300

    modifica o testeShield para ler um vetor
    O testeShield agora lê um vetor com valores fixos de 0*vcc; 0.25*vcc; 0.5*vcc; 0.75*vcc; e vcc, ao invés de ler comandos do serial.

--- testeShields/testeShields.ino
+++ testeShields/testeShields.ino
@@ -2,7 +2,7 @@
   Teste shields de motor VSSS
 
   Verifica se um shield apresenta o funcionamento correto:
-    - Através do monitor serial, envia-se o sentido e o valor pwm para as rodas
+    - Através de dois vetores obtem-se o sentido e o valor pwm para as rodas
       -> Sentido D (direita) ou E (esquerda)
       -> Valor de 0 a 255 (verificar com o voltímetro se a tensão nos terminais equivale ao pwm)
     - Verificar qual o sentido em relação a qual pino fica em HIGH (Xle e Xri)
@@ -20,6 +20,8 @@ dir_left  ->  Frente = HIGH   Trás = LOW
 dir_right ->  Frente = LOW    Trás = HIGH
 */
 
+int values[] = {0, 64, 128, 192, 255};
+
 void setup() {
   Serial.begin(4800);
   pinMode(dir_right, OUTPUT);       //configurando o pino 'dir_right' como  saida
@@ -31,64 +33,51 @@ void setup() {
 }
 
 void loop() {
-
-  char cmd = 0;
-
-  if(Serial.available() >= 2){
-    cmd = Serial.read();                  //Lê o comando do robô, sendo 'F', 'T', 'D' ou 'E', explicação de cada comando no switch
-    signed int value = Serial.read();     //Lê o valor do comando, pegando o valor como um char e convertendo para inteiro.
-    switch (cmd)
-    {
-      /*case 'T': // Comando para andar para tras
-        {
-          digitalWrite(dir_right, LOW);
-          digitalWrite(dir_left, HIGH);
-          analogWrite(pwm_right, value);
-          analogWrite(pwm_left, value);
-          delay(100);
-          break;
-        }
-      case 'F': // Comando para andar para frente
-        {
-          digitalWrite(dir_right, HIGH);
-          digitalWrite(dir_left, LOW);
-          analogWrite(pwm_right, value);
-          analogWrite(pwm_left, value);
-          delay(100);
-          break;
-        }*/
-      case 'E': // Comando para andar para esquerda
-        {
-          digitalWrite(dir_right, HIGH);
-          digitalWrite(dir_left, HIGH);
-          analogWrite(pwm_right, value);
-          analogWrite(pwm_left, 0);
-          delay(100);/*
-          digitalWrite(dir_right, LOW);
-          digitalWrite(dir_left, HIGH);
-          analogWrite(pwm_right, 255);
-          analogWrite(pwm_left, 255);
-          delay(value*10);*/
-          break;
-        }
-
-      case 'D': // Comando para andar para direita
-        {
-          digitalWrite(dir_right, LOW);
-          digitalWrite(dir_left, LOW);
-          analogWrite(pwm_right, 0);
-          analogWrite(pwm_left, value);
-          delay(100);/*
-          digitalWrite(dir_right, LOW);
-          digitalWrite(dir_left, HIGH);
-          analogWrite(pwm_right, 255);
-          analogWrite(pwm_left, 255);
-          delay(value*10);*/
-          break;
-        }
-
-
+  //char cmd = 'D';
+  char cmd = 'E';
+  for(int j=0; j<=4; j++){
+      int value = values[j];
+      switch (cmd)
+      {
+        /*case 'T': // Comando para andar para tras
+          {
+            digitalWrite(dir_right, LOW);
+            digitalWrite(dir_left, HIGH);
+            analogWrite(pwm_right, value);
+            analogWrite(pwm_left, value);
+            delay(100);
+            break;
+          }
+        case 'F': // Comando para andar para frente
+          {
+            digitalWrite(dir_right, HIGH);
+            digitalWrite(dir_left, LOW);
+            analogWrite(pwm_right, value);
+            analogWrite(pwm_left, value);
+            delay(100);
+            break;
+          }*/
+        case 'E': // Comando para andar para esquerda
+          {
+            digitalWrite(dir_right, HIGH);
+            digitalWrite(dir_left, HIGH);
+            analogWrite(pwm_right, value);
+            analogWrite(pwm_left, 0);
+            delay(5000);
+            break;
+          }
+  
+        case 'D': // Comando para andar para direita
+          {
+            digitalWrite(dir_right, LOW);
+            digitalWrite(dir_left, LOW);
+            analogWrite(pwm_right, 0);
+            analogWrite(pwm_left, value);
+            delay(5000);
+            break;
+          }
+      }
+    }
     analogWrite(pwm_right, 0);        //fazendo o pino pwm_right ficar com o valor 0
     analogWrite(pwm_left, 0);
-  }
 }
